776e3bc814bd7baef51af4f25a60a765da02a902
ivan setiawan <ivansetiawanvll@gmail.com>
2021-09-21 05:29:07 +0700

    url

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Routing\UrlGenerator;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
