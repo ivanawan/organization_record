50ca0646c45d94ab1f56b48ba215fc48e78ce506
ivan setiawan <ivansetiawanvll@gmail.com>
2021-09-23 09:22:55 +0700

    veryfi off

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
+// use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Model
 {
     use HasFactory;
 
